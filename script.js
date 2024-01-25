2ecc31257bb016d044181d071169c9a4bf2e728b
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-01-24 19:47:02 -0500

    edited

---
+++ script.js
@@ -0,0 +1,64 @@
+const questions = [
+    {
+        question: "What is coding?"
+        options: ["arrow", "math tool", "none of the above", "data tructure that can store a fixed size collection of elements with the same data type"],
+        answer: "data tructure that can store a fixed size collection of elements with the same data type"
+    },
+
+    {
+        question: "What does JS stand for in programming?"
+        options: ["Jackson state", "Javascript", "none of the above", "Jay Sean"],
+        answer: "Javascript"
+
+
+
+    },
+
+    {
+        question: "A variable inside a fucntion is _?"
+        options: ["Outside", "Inside", "Local", "Global"],
+        answer: "Local"
+
+
+    }
+
+        question: "What is the style sheet or file used for?"
+        options: ["nothing", "styles the website by adding color, fonts, size, etc", "adds the writing only", "it isn't used in programming"],
+    answer: "styles the website by adding color, fonts, size, etc"
+
+
+    }
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+Function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+
+    Function showQuestion() {
+        let currentQuestion = questions[currentQuestionIndex];
+        let questionNo = currentQuestionIndex + 1;
+        questionElement.innerHTML = questionNo + "." + currentQuestion.
+            question;
+
+        currentQuestion.answers.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+            answerButton.appendChild(button);
+        });
+
+
+    }
+
+
+}
