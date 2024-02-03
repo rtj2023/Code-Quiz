e0a1678e53b99ecf0c25904539ed5fd91df7e2b4
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-02-03 17:48:42 -0500

    updated

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 const questions = [
     {
-        question: "What is coding?"
+        question: "What is coding?",
         options: ["arrow", "math tool", "none of the above", "data tructure that can store a fixed size collection of elements with the same data type"],
         answer: "data tructure that can store a fixed size collection of elements with the same data type"
     },
-
     {
-        question: "What does JS stand for in programming?"
+        question: "What does JS stand for in programming?",
         options: ["Jackson state", "Javascript", "none of the above", "Jay Sean"],
         answer: "Javascript"
 
 
 
     },
-
     {
-        question: "A variable inside a fucntion is _?"
+        question: "A variable inside a fucntion is _?",
         options: ["Outside", "Inside", "Local", "Global"],
         answer: "Local"
+    }, {
 
-
-    }
-
-        question: "What is the style sheet or file used for?"
+        question: "What is the style sheet or file used for?",
         options: ["nothing", "styles the website by adding color, fonts, size, etc", "adds the writing only", "it isn't used in programming"],
-    answer: "styles the website by adding color, fonts, size, etc"
-
-
+        answer: "styles the website by adding color, fonts, size, etc"
     }
-
 ];
+
+
 
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
@@ -38,21 +33,23 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-Function startQuiz(){
+let btnIndex = 0
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 
-    Function showQuestion() {
+    function showQuestion() {
         let currentQuestion = questions[currentQuestionIndex];
         let questionNo = currentQuestionIndex + 1;
         questionElement.innerHTML = questionNo + "." + currentQuestion.
             question;
 
-        currentQuestion.answers.forEach(answer => {
+        currentQuestion.options.forEach(answer => {
+            btnIndex++
             const button = document.createElement("button");
-            button.innerHTML = answer.text;
+            button.innerHTML = questions[currentQuestionIndex].options[btnIndex];
             button.classList.add("btn");
             answerButton.appendChild(button);
         });
@@ -60,5 +57,12 @@ Function startQuiz(){
 
     }
 
+    function resetState() {
+        nextButtton.style.display = "none"
+        while (answerButtons.firstChild) {
+            answerButtons.removeChild(answerButtons.firstChild);
+        }
 
-}
+
+    }
+    startQuiz();
