bf8654a37b4cc7047d555798473ae74e5396a102
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-02-08 21:57:36 -0500

    updated

--- script.js
+++ script.js
@@ -104,4 +104,22 @@ nextButton.addEventListener("click", () => {
     }
 })
 
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from answerButtons.children.forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
 startQuiz();
