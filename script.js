153ab10b0174f981edec6514e3e9c08b223738e4
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-02-20 10:40:37 -0500

    updated

--- script.js
+++ script.js
@@ -46,15 +46,21 @@ function showQuestion() {
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + "." + currentQuestion.
         question;
+
     answerButtons.innerHTML = ""
+    let correctAnswer = currentQuestion.answer
     currentQuestion.options.forEach(answer => {
 
         const button = document.createElement("button");
         button.innerHTML = answer;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
+
+
+
+        if (correctAnswer == answer) {
+            button.dataset.correct = true;
+
         }
         button.addEventListener("click", selectAnswer);
 
@@ -74,7 +80,10 @@ function resetState() {
 
 function selectAnswer(e) {
     const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "correct";
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    console.log(selectedBtn)
+
+    console.log(isCorrect)
     if (isCorrect)
         selectedBtn.classList.add("correct");
     else
@@ -127,22 +136,5 @@ nextButton.addEventListener("click", () => {
     }
 })
 
-function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect) {
 
-        selectedBtn.classList.add("correct");
-    } else {
-        selectedBtn.classList.add("incorrect");
-    }
-    Array.from(answerButtons.children).forEach(button => {
-        if (button.dataset.correct === "true") {
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
-    nextButton.style.display = "block";
-
-}
 startQuiz();
